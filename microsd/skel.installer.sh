769abe826237bede69e7c7e299a9cdfb1356e71a
mindego <mindego@mail.ru>
2015-11-22 12:11:47 +0400

    Modified default installer for support of unchanged rootbind FS and kernels.

--- microsd/skel.installer.sh
+++ microsd/skel.installer.sh
@@ -10,10 +10,11 @@ TARBALL_DEVICE=/dev/mmcblk1p1
 TARBALL=wily.mate.tar.gz
 KERNEL=kernel.blob
 INSTALL_DEVICE=/dev/mmcblk0p7
-INSTALL_DIRECTORY=/linuxroot/ubuntu.wily
-SYMLINK_ROOT=/linux.root
+INSTALL_DIRECTORY=ubuntu.wily
+ROOTFS_FIRECTORY=linux.root
+SYMLINK_ROOT=linuxroot
 
-echo -e "Installing $NAME to $INSTALL_DIRECTORY on $INSTALL_DEVICE from $TARBALL image on $TARBALL_DEVICE"
+echo -e "Installing $NAME to $ROOTFS_DIRECTORY/$INSTALL_DIRECTORY on $INSTALL_DEVICE from $TARBALL image on $TARBALL_DEVICE"
 
 # Mount the root filesystem
 echo Mounting data partition...
@@ -24,12 +25,12 @@ mount -t ext4 -o noatime,nodiratime,errors=panic $INSTALL_DEVICE /mnt/root
 # mount $TARBALL_DEVICE /mnt/sdcard
 
 echo Creating rootfs directories...
-mkdir -p /mnt/root/$INSTALL_DIRECTORY
-chmod 755 /mnt/root/$INSTALL_DIRECTORY
+mkdir -p /mnt/root/$ROOTFS_DIRECTORY/$INSTALL_DIRECTORY
+chmod 755 /mnt/root/$ROOTFS_DIRECTORY/$INSTALL_DIRECTORY
 rm /mnt/root/$SYMLINK_ROOT
 #mkdir -p /mnt/root/$SYMLINK_ROOT
 #chmod 755 /mnt/root/$SYMLINK_ROOT
-cd /mnt/root/$INSTALL_DIRECTORY
+cd /mnt/root/$ROOTFS_DIRECTORY/$INSTALL_DIRECTORY
 
 echo Unpacking rootfs...
 #tar -pxvzf /mnt/sdcard/$TARBALL
@@ -40,6 +41,6 @@ dd if=/mnt/sdcard/$KERNEL of=/dev/mmcblk0p4
 
 echo Symlinking rootfs directory...
 cd /mnt/root
-ln -s linuxroot/`basename $INSTALL_DIRECTORY` `basename $SYMLINK_ROOT`
+ln -s $ROOTFS_DIRECTORY/$INSTALL_DIRECTORY $SYMLINK_ROOT
 
 echo $NAME is successfully installed.
